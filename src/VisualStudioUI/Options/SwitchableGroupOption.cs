faf9f9cc8b109f1fe3f94b8d7f2ae0507e3ea3ef
marc0090 <380746161@qq.com>
2021-06-22 16:53:50 +0800

    rename property

--- src/VisualStudioUI/Options/SwitchableGroupOption.cs
+++ src/VisualStudioUI/Options/SwitchableGroupOption.cs
@@ -10,30 +10,28 @@ using System;
 using System.Collections.Generic;
 using Microsoft.VisualStudioUI.Options.Models;
 
-namespace Microsoft.VisualStudioUI.Options
-{
-    public class SwitchableGroupOption : Option
-    {
+namespace Microsoft.VisualStudioUI.Options {
+	public class SwitchableGroupOption : Option {
 
-        public ViewModelProperty<bool> State { get; }
+		public ViewModelProperty<bool> IsOn { get; }
 
-        public event EventHandler? SwitchChanged;
+		public event EventHandler? SwitchChanged;
 
-        public readonly List<Option> _childOptions = new List<Option>();
+		public readonly List<Option> _childOptions = new List<Option> ();
 
-        public IReadOnlyList<Option> ChildOptions => _childOptions;
+		public IReadOnlyList<Option> ChildOptions => _childOptions;
 
-        public void AddOption(Option option) => _childOptions.Add(option);
+		public void AddOption (Option option) => _childOptions.Add (option);
 
-        public void SwitchChangedInvoke(object sender, EventArgs e)
-        {
-            SwitchChanged?.Invoke(sender, e);
-        }
+		public void SwitchChangedInvoke (object sender, EventArgs e)
+		{
+			SwitchChanged?.Invoke (sender, e);
+		}
 
-        public SwitchableGroupOption(ViewModelProperty<bool> state)
-        {
-            State = state;
-            Platform = OptionFactoryPlatform.Instance.CreateSwitchableGroupOptionPlatform(this);
-        }
-    }
+		public SwitchableGroupOption (ViewModelProperty<bool> isOn)
+		{
+			IsOn = isOn;
+			Platform = OptionFactoryPlatform.Instance.CreateSwitchableGroupOptionPlatform (this);
+		}
+	}
 }
