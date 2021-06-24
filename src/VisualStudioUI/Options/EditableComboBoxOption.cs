79b1b7016ebe84a9ee8e68536a7015090cebb4c0
Bret Johnson <bret.johnson@microsoft.com>
2021-06-24 00:31:41 -0400

    Add some doc and make some code style cleanups

--- src/VisualStudioUI/Options/EditableComboBoxOption.cs
+++ src/VisualStudioUI/Options/EditableComboBoxOption.cs
@@ -12,11 +12,20 @@ using Microsoft.VisualStudioUI.Options.Models;
 
 namespace Microsoft.VisualStudioUI.Options
 {
+    /// <summary>
+    /// An EditableComboBoxOption shows a list of items, where the user must choose one or
+    /// the user can provide an arbitrary value, not in the list.
+    /// </summary>
     public class EditableComboBoxOption : Option
     {
         public ViewModelProperty<string> Property { get; }
         public ViewModelProperty<ImmutableArray<string>> ItemsProperty { get; }
 
+        /// <summary>
+        /// Create an EditableComboBox.
+        /// </summary>
+        /// <param name="property">current value; it can be the empty string to indicate nothing is chosen</param>
+        /// <param name="itemsProperty">list of items to show</param>
         public EditableComboBoxOption(ViewModelProperty<string> property, ViewModelProperty<ImmutableArray<string>> itemsProperty)
         {
             Property = property;
