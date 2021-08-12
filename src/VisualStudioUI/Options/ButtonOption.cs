3dfb2c08d1d43dd0cfd609705c09de5959d5262c
marc0090 <380746161@qq.com>
2021-08-12 10:47:00 +0800

    simplify button option

--- src/VisualStudioUI/Options/ButtonOption.cs
+++ src/VisualStudioUI/Options/ButtonOption.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using Microsoft.VisualStudioUI.Options.Models;
 
 namespace Microsoft.VisualStudioUI.Options
 {
@@ -8,34 +7,17 @@ namespace Microsoft.VisualStudioUI.Options
     /// </summary>
     public class ButtonOption : Option
     {
-        public enum ButtonType
-        {
-            Normal = -1,
-            CheckBox = 3, // NSButtonType.Switch
-            Radio = 4, // NSButtonType.Radio
-        }
+        public string ButtonLabel { get; set; } = "";
 
-        public ButtonType Type { get; }
-
-        public ViewModelProperty<bool> Active { get; set; }
-        public ViewModelProperty<bool> Hidden { get; set; }
-
-        public event EventHandler Actived;
         public event EventHandler Clicked;
-
-        public void UpdateStatus(object sender, EventArgs e)
-        {
-            Actived?.Invoke(sender, e);
-        }
 
         public void ButtonClicked(object sender, EventArgs e)
         {
             Clicked?.Invoke(sender, e);
         }
 
-        public ButtonOption(ButtonType btnType = ButtonType.Normal)
+        public ButtonOption()
         {
-            Type = btnType;
             Platform = OptionFactoryPlatform.Instance.CreateButtonOptionPlatform(this);
         }
     }
