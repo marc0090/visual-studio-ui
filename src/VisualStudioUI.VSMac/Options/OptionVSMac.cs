a2afd03860335f38c7a0e2856578f40f9790a1ef
marc0090 <37265150+marc0090@users.noreply.github.com>
2021-09-18 15:16:53 +0800

    Fix leftlabel localization width (#42)
    * maximum 2 lines of left lable
    
    * fix Bug 1393634: [VSM] The text is cut off on Preferences->Android page
    
    * Update Main.cs

--- src/VisualStudioUI.VSMac/Options/OptionVSMac.cs
+++ src/VisualStudioUI.VSMac/Options/OptionVSMac.cs
@@ -32,27 +32,33 @@ namespace Microsoft.VisualStudioUI.VSMac.Options
 
             // View:     label
             var label = new NSTextField();
+            label.Cell = new VerticallyCenteredTextFieldCell(yOffset:-1);
             label.Editable = false;
             label.Bordered = false;
+            
             label.DrawsBackground = false;
             label.PreferredMaxLayoutWidth = 1;
+
             // TODO: Make the colon be localization friendly
             label.StringValue = labelString + ":";
-            label.HeightAnchor.ConstraintEqualToConstant(16f).Active = true;
 
             label.Alignment = NSTextAlignment.Right;
             label.TextColor = NSColor.LabelColor;
             label.TranslatesAutoresizingMaskIntoConstraints = false;
-
-            if(labelString.Length < 32)
+            label.MaximumNumberOfLines = 2;
+            if (labelString.Length < 32)
             {
                 label.Font = NSFont.SystemFontOfSize(NSFont.SystemFontSize);
             }
+            else if(labelString.Length < 38)
+            {
+                label.Font = NSFont.SystemFontOfSize(12f);
+            }
             else
             {
-                label.Font = NSFont.SystemFontOfSize(11f);
+                label.Font = NSFont.SystemFontOfSize(10f);
             }
-
+            label.SizeToFit();
             label.WidthAnchor.ConstraintGreaterThanOrEqualToConstant(213f).Active = true;
 
             return label;
